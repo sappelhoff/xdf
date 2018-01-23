871b7f42a47e72c245b1f0114794e94b521a480a
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-01-23 13:48:02 +0100

    got rid of unnecessary stuff - good to go for pip install?

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='pyxdf',
+      version='1.14',
+      description='XDF: General-purpose container format for multi-channel time series data with extensive associated meta information',
+      url='https://github.com/sccn/xdf',
+      author='Chadwick Boulay et al.',
+      author_email='chadwick.boulay@gmail.com',
+      license='',
+      packages=['pyxdf'],
+      zip_safe=False)

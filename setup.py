bc0e7ee519748dcc8c0ba702357581d687aea9dc
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-01-31 09:27:31 +0100

    license is not known

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 
 setup(name='pyxdf',
       version='1.14',
-      description='XDF: General-purpose container format for multi-channel time series data with extensive associated meta information',
+      description='XDF: General-purpose container format for multi-channel'
+                  ' time series data with extensive associated meta'
+                  ' information',
       url='https://github.com/sccn/xdf',
       author='Chadwick Boulay et al.',
       author_email='chadwick.boulay@gmail.com',
-      license='',
+      license='not known',
       packages=['pyxdf'],
       zip_safe=False)

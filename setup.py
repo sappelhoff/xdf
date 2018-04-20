d31613dab487de58d4722a06bd16a6add7aea6f0
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-20 19:45:45 +0200

    update author and lic

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup
 
 setup(name='pyxdf',
       version='1.14',
-      description='XDF: General-purpose container format for multi-channel'
-                  ' time series data with extensive associated meta'
-                  ' information',
-      url='https://github.com/sccn/xdf',
-      author='Chadwick Boulay et al.',
-      author_email='chadwick.boulay@gmail.com',
-      license='not known',
-      packages=['pyxdf'],
-      zip_safe=False)
+      description=('XDF: General-purpose container format for multi-channel'
+                   ' time series data with extensive associated meta'
+                   ' information'),
+      url='https://github.com/sccn/xdf/tree/master/Python',
+      author='Christian Kothe',
+      author_email='kothe@sccn.ucsd.edu',
+      license='BSD',
+      packages=['pyxdf'])
